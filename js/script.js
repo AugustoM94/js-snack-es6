14c182a978867aa0988f7f4791c61497d48dcffc
AugustoM94 <email@github.com>
2023-10-25 16:21:29 +0200

    added snack4 added(array and  puntiFatti and falliFatti

--- js/script.js
+++ js/script.js
@@ -132,3 +132,65 @@ const segnaposti = listaVip.map((ospite, index) => ({
   }
 
   console.log(`La bici con peso minore è ${biciPesoMinore.name} con un peso di ${biciPesoMinore.peso} kg.`);
+
+
+
+  /*
+SNACK 4
+Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti.
+Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+BONUS 
+Stampare in pagina oltre che in console!*/
+
+
+//creato array con squadre di calcio
+const footballTeam = [
+
+    {
+        name: 'Juventus',
+        puntiFatti: 0,
+        falliFatti: 0,
+    },
+    {
+        name: 'Milan',
+        puntiFatti: 0,
+        falliFatti: 0,
+    },
+    {
+        name: 'Lazio',
+        puntiFatti: 0,
+        falliFatti: 0,
+    },
+    {
+        name: 'Lipsia',
+        puntiFatti: 0,
+        falliFatti: 0,
+    },
+    {
+        name: 'Arsenal',
+        puntiFatti: 0,
+        falliFatti: 0,
+    },
+    {
+        name: 'Barcellona',
+        puntiFatti: 0,
+        falliFatti: 0,
+    },
+]
+
+for (let i = 0; i < footballTeam.length; i++) {
+    footballTeam[i].puntiFatti = getRandomNumber(0, 114);
+    footballTeam[i].falliFatti = getRandomNumber(0, 1000); 
+}
+
+console.log(footballTeam)
+
+
+
+
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
